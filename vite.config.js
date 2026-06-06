ab6e1d013c5a00b5de5db6bc78c1d2b227a9bab1
georgku <kummertgeorg@gmail.com>
2026-06-07 00:15:37 +0200

    Fix case sensitive resolve

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    caseSensitive: false
+  }
+})
+
 
 // https://vite.dev/config/
 export default defineConfig({
